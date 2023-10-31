a7e02702cb9f1efcc30b2bace07de1e920fb75a6
LarryCao <wenloncao2@gmail.com>
2023-10-31 22:06:52 +0000

    Add the generic exchange class

--- aat/exchange/exchange.py
+++ aat/exchange/exchange.py
@@ -8,4 +8,45 @@ from .base.order_entry import _OrderEntry
 
 
 class Exchange(_MarketData, _OrderEntry):
-    pass
+    """Generic representation of an exchange. There are two primary functionalities of an exchange.
+
+    Market Data Source:
+        exchanges can stream data to the engine
+
+    Order Entry Sink:
+        exchanges can be queried for data, or send data
+    """
+
+    def __init__(self, exchange: ExchangeType) -> None:
+        self._exchange: ExchangeType = exchange
+
+    def exchange(self) -> ExchangeType:
+        return self._exchange
+    
+    @abstractmethod
+    async def connect(self) -> None:
+        """Connect to exchange. should be asynchronous.
+        
+        For OrderEntry-only, can just return None
+        """
+
+    async def lookup(self, instrument: Instrument) -> List[Instrument]:
+        """look up an instrument on the exchange"""
+        return []
+    
+    # ****************** #
+    # Inherited  methods #
+
+    # From _MarketData
+    #
+    # async def tick(self):
+    # def instruments(self):
+    # def subscribe(self, instrument):
+
+    # From _OrderEntry
+    #
+    # async def newOrder(self, order: Order):
+    # def accounts(self) -> List:
+    # ************************** #
+        
+    
