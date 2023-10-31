baba8bf591bb3b49f065891aa32f9257a3d4a29d
LarryCao <wenloncao2@gmail.com>
2023-10-31 01:27:22 +0000

    Add exchange to core

---
+++ aat/core/instrument/calendar.py
@@ -0,0 +1,9 @@
+from datetime import time
+from typing import Optional, Tuple, Union, cast
+
+from aat.common import AATException
+from pytz import UTC
+
+class TradingDay(object):
+    """
+    """
